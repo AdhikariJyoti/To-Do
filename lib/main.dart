62d42529965cf7516224282b6377b192ac7f8d2a
Jyoti Adhikari <adkjyoti55@gmail.com>
2021-11-02 23:37:37 +0545

    Files Added

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+void main()=> runApp(MaterialApp(
+  title: "TO-DO APP",
+  home: TODOAPP(),
+));
+class TODOAPP extends StatefulWidget {
+
+
+  @override
+  _TODOAPPState createState() => _TODOAPPState();
+}
+
+class _TODOAPPState extends State<TODOAPP> {
+  late String value;
+
+  var listTODO =[''];
+  addTODO(String item){
+    setState(() {
+      listTODO.add(item);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TODO APP"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed:(){
+          addTODO(value);
+        },
+      ),
+      body: ListView.builder(
+          itemCount: listTODO.length,
+          itemBuilder: (context, index){
+        return index==0 ? Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: "Add item"
+            ),
+
+            onChanged: (val){
+              value=val;
+            },
+          ),
+        ):ListTile(
+          leading: Icon(Icons.work),
+          title: Text('${listTODO[index]}',style: TextStyle(fontWeight: FontWeight.bold),),
+
+        );
+
+      })
+
+
+    );
+  }
+}
